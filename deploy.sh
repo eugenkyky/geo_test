d93134d9b6948e13adb3c7143b512111a18f24f3
eugenkyky <eugenkyky@gmail.com>
2016-08-05 14:38:10 +0500

    Vagrant changes

--- deploy.sh
+++ deploy.sh
@@ -6,10 +6,10 @@ sudo LC_ALL=C.UTF-8 add-apt-repository ppa:ondrej/php -y &&
 sudo apt-get update &&
 sudo apt-get install php7.0 curl git libpcre3 zip unzip php7.0-xml php-mbstring php7.0-sqlite -y &&
 (curl -sS https://getcomposer.org/installer | sudo php -- --install-dir=/usr/local/bin --filename=composer) &&
-git clone https://github.com/eugenkyky/test_repo.git test_assignment &&
+git clone https://github.com/eugenkyky/geo_test.git test_assignment &&
 #composer get additional packages
 cd test_assignment &&
-sudo composer install &&
+composer install &&
 #create schema
 php artisan migrate &&
 #create users dir
